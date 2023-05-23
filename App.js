c53e587abcdf527312395eff4c1e6a1060ae1c5b
Naitik Rathod <naitikrathod@Naitiks-MacBook-Air.local>
2023-05-23 14:04:55 -0400

    basic form and button done

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding:50}}> 
+      <Text
+      style ={{textAlign: "center",
+              fontSize:15,
+              fontWeight:"bold",
+              marginBottom:20,
+              marginTop:20
+              }}>
+      Hello Jack, this is your Cat Track App
+      </Text>
+
+      <TextInput 
+        style={{borderWidth:1 , 
+                borderColor:"black",
+                height:25,
+                marginBottom:15,
+                paddingLeft:5}} 
+        placeholder="Cat's Name"/>
+
+      <TextInput 
+        style={{borderWidth:1 , 
+                borderColor:"black",
+                height:25,
+                marginBottom:15,
+                paddingLeft:5}} 
+        placeholder="Breed" />
+
+      <TextInput 
+        style={{borderWidth:1 , 
+                borderColor:"black",
+                height:25,
+                marginBottom:15,
+                paddingLeft:5}} 
+        placeholder="Short Description" />
+      
+      <TextInput 
+        style={{borderWidth:1 , 
+                borderColor:"black",
+                height:25,
+                marginBottom:15,
+                paddingLeft:5}} 
+        placeholder="Image will come here" />
+      <View style={styles.button} >
+      <TouchableOpacity >
+        <Text>Add Cat to the List</Text>
+      </TouchableOpacity>
+      </View>
+      
+      <Text
+      style ={{textAlign: "center",
+              fontSize:15,
+              fontWeight:"bold",
+              marginBottom:20,
+              marginTop:40
+              }}>
+      List of Your Cats
+      </Text>
+
     </View>
   );
 }
@@ -17,4 +73,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  }
 });
